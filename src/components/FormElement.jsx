a2733a855a4c75abee698852b6a9f9240362e320
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-04-29 03:59:48 +0530

    Updated formelement

--- src/components/FormElement.jsx
+++ src/components/FormElement.jsx
@@ -3,12 +3,25 @@ import React, { useState, useEffect }from 'react';
 export default () => {
     const [data, updateData] = useState(''); 
     useEffect(() => {
-        /* document.getElementById('output').innerText = data; */
-        fetch('http://localhost:5000')
-
+            document.getElementById('output').innerText = "Default value of data state variable.";
     })
     function sendData() {
-        console.log("sending data");
+        // Typing in an absolute URL for now. Will need to use relative URL (webpack bundler technique will help - refer link below)
+        /** After you checked that your reactapp works well after integration with your node server 
+         * Just refer the link below to make your react code and nodejs app code to make it production level.
+         * (refer: https://www.newline.co/fullstack-react/articles/using-create-react-app-with-a-server/)
+        */
+        fetch('http://localhost:5000', { 
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
+        .then(resp => resp.text())
+        .then( (resp) => {
+            updateData(resp);
+        })
+        .catch( (e) => {
+            console.log(`error at useEffect:\n ${e}`);
+        })
     }
     return(
         <div>
