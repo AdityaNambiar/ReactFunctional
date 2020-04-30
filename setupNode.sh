8cb485e0675b22174a0a8c7b6adaa9607f1ca048
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-05-01 04:11:17 +0530

    Added a script to setup Node in Jenkins CI server

---
+++ setupNode.sh
@@ -0,0 +1,11 @@
+# Install all packages essential for setting up node environment in Jenkins container:
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm --version
+node --version
+npm --version
