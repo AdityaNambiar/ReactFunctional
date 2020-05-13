8b12ff2a256d99333458dd3416de3d83a11df2ac
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-05-13 21:23:23 +0530

    Fixed test case which broke the build on devopschain

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/datum state/i);
+  const linkElement = getByText(/data state/i);
   expect(linkElement).toBeInTheDocument();
 });
