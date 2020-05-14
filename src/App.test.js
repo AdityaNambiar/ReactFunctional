d49cfb46b642f058804a82f76b1c1d02f5de42d0
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-05-14 22:48:46 +0530

    Purposefully modified testcase - its incorrect and gives error

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/data state/i);
+  const linkElement = getByText(/datum state/i);
   expect(linkElement).toBeInTheDocument();
 });
