db33060e3bf97e46035bb17c3cafe6c83c56ac0c
AdityaNambiar <aditya.nambiar24@gmail.com>
2020-05-14 21:36:59 +0530

    Fixed webserver index and Dockerfile

--- webserver/index.js
+++ webserver/index.js
@@ -10,9 +10,9 @@ const port = 8080;
 app.use(bodyParser.json())
 app.use(cors())
 
-app.use(express.static(path.join(__dirname,'..','build')));
+app.use(express.static(path.join(__dirname,'build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname,'..','build'));
+  res.sendFile(path.join(__dirname,'build'));
 });
 
 /**
